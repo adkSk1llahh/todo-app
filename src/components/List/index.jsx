d7757161e0c8a85db1b77bc9b475f270fde4a2dd
Adilkhan Zhumabekov <sk1llahh2@gmail.com>
2021-07-21 23:11:41 +0600

    view tasks on click

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,55 +1,46 @@
-import React from 'react';
-<<<<<<< HEAD
-import axios from 'axios';
-import classNames from 'classnames';
+import React from "react";
+import axios from "axios";
+import classNames from "classnames";
 
-=======
-import classNames from 'classnames';
->>>>>>> 8ea5311b92f0343fd372e8101f64f3253577027a
-import removeSvg from '../../assets/img/remove.svg'
+import removeSvg from "../../assets/img/remove.svg";
 
-import Badge from '../Badge';
+import Badge from "../Badge";
 
-import './List.scss';
+import "./List.scss";
 
-
-const List = ({ items, isRemovable, onClick, onRemove }) =>{
-
-    const removeList = (item) => {
-        if(window.confirm('Вы действиетльно хотите удалить список?')){
-<<<<<<< HEAD
-            axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
-                onRemove(item.id);
-              });
-=======
-            onRemove(item);
->>>>>>> 8ea5311b92f0343fd372e8101f64f3253577027a
-        }
+const List = ({ items, isRemovable, onClick, onRemove, onClickItem, activeItem}) => {
+  const removeList = (item) => {
+    if (window.confirm("Вы действиетльно хотите удалить список?")) {
+      axios.delete("http://localhost:3001/lists/" + item.id).then(() => {
+        onRemove(item.id);
+      });
     }
+  };
 
-    return (
+  return (
     <ul onClick={onClick} className="list">
-        {
-            items.map((item, index) => (
-            <li key={index} className={classNames(item.className, {active: item.active})}>
-<<<<<<< HEAD
-                <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
-=======
-                <i>
-                   {item.icon ? (item.icon) : ( <Badge color={item.color} /> )}
-                </i>
->>>>>>> 8ea5311b92f0343fd372e8101f64f3253577027a
-                <span>{item.name}</span>
-                {isRemovable && <img className="list__remove_icon" src={removeSvg} alt="remove icon" onClick={()=>removeList(item)}/>}
-            </li> 
-            
-            ))
-        }
-       
+      {items.map((item, index) => (
+        <li
+          key={index}
+          className={classNames(item.className, { active: activeItem && activeItem.id === item.id })}
+          onClick={onClickItem ? () => onClickItem(item) : null}
+        >
+          <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
+          <span>
+            {item.name} {item.tasks && ` (${item.tasks.length})`}
+          </span>
+          {isRemovable && (
+            <img
+              className="list__remove_icon"
+              src={removeSvg}
+              alt="remove icon"
+              onClick={() => removeList(item)}
+            />
+          )}
+        </li>
+      ))}
     </ul>
-    );
-}; 
+  );
+};
 
 export default List;
-
-
--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -2,24 +2,18 @@ import React from "react";
 import "./Tasks.scss";
 import editSvg from "../../assets/img/edit.svg";
 
-<<<<<<< HEAD
 const Tasks = ({ list }) => {
   return (
     <div className="tasks">
       <h2 className="tasks__title">
         {list.name}
-=======
-const Tasks = () => {
-  return (
-    <div className="tasks">
-      <h2 className="tasks__title">
-        Frontend
->>>>>>> 8ea5311b92f0343fd372e8101f64f3253577027a
-        <img src={editSvg} alt="edit icon" />
+        <img onClick={()=>{
+          
+        }} src={editSvg} alt="edit icon" />
       </h2>
 
       <div className="tasks__items">
-<<<<<<< HEAD
+        {!list.tasks.length && <h2>Задачи отсутсвуют</h2>}
         {list.tasks.map((task) => (
           <div key={task.id} className="tasks__items-row">
             <div className="checkbox">
@@ -45,32 +39,6 @@ const Tasks = () => {
             <input readOnly value={task.text} />
           </div>
         ))}
-=======
-        <div className="tasks__items-row">
-          <div className="checkbox">
-            <input id="check" type="checkbox" />
-            <label htmlFor="check">
-              <svg
-                width="11"
-                height="8"
-                viewBox="0 0 11 8"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001"
-                  stroke="#000"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </label>
-          </div>
-
-          <p>Qweqweqweqweqweqwe qweqweqwe qweqweqwe</p>
-        </div>
->>>>>>> 8ea5311b92f0343fd372e8101f64f3253577027a
       </div>
     </div>
   );
