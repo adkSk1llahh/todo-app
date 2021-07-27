fd01457a1602d738b1051cf0647916f0c7dcede8
Adilkhan Zhumabekov <sk1llahh2@gmail.com>
2021-07-27 21:48:51 +0600

    link to tasks

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 import "./Tasks.scss";
 import AddTaskForm from "./AddTaskForm";
@@ -7,7 +8,15 @@ import Task from "./Task";
 
 import editSvg from "../../assets/img/edit.svg";
 
-const Tasks = ({ list, onEditTitle, onAddTask, withoutEmpty, onRemoveTask, onEditTask, onCompleteTask }) => {
+const Tasks = ({
+  list,
+  onEditTitle,
+  onAddTask,
+  withoutEmpty,
+  onRemoveTask,
+  onEditTask,
+  onCompleteTask,
+}) => {
   const editTitle = () => {
     const newTitle = window.prompt("Название списка", list.name);
     if (newTitle) {
@@ -22,19 +31,29 @@ const Tasks = ({ list, onEditTitle, onAddTask, withoutEmpty, onRemoveTask, onEdi
     }
   };
 
-
   return (
     <div className="tasks">
-      <h2 style={{ color: list.color.hex }} className="tasks__title">
-        {list.name}
-        <img onClick={editTitle} src={editSvg} alt="edit icon" />
-      </h2>
-
+      <Link to={`/lists/${list.id}`}>
+        <h2 style={{ color: list.color.hex }} className="tasks__title">
+          {list.name}
+          <img onClick={editTitle} src={editSvg} alt="edit icon" />
+        </h2>
+      </Link>
       <div className="tasks__items">
-        {!withoutEmpty && list.tasks && !list.tasks.length && <h2>Задачи отсутсвуют</h2>}
-        {list.tasks && list.tasks.map((task) => (
-          <Task key={task.id} list={list} onEdit={onEditTask} onRemove={onRemoveTask} onComplete = {onCompleteTask} {...task} />
-        ))}
+        {!withoutEmpty && list.tasks && !list.tasks.length && (
+          <h2>Задачи отсутсвуют</h2>
+        )}
+        {list.tasks &&
+          list.tasks.map((task) => (
+            <Task
+              key={task.id}
+              list={list}
+              onEdit={onEditTask}
+              onRemove={onRemoveTask}
+              onComplete={onCompleteTask}
+              {...task}
+            />
+          ))}
         <AddTaskForm key={list.id} list={list} onAddTask={onAddTask} />
       </div>
     </div>
